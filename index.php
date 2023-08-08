b002b0a33c0498d043efc4d4b06b957d00cd77fc
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-08-08 11:49:35 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Discover the Power of Chainsaws with Chainsaw Guru</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 16px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Discover the Power of Chainsaws with Chainsaw Guru!</h1>
+        <p>This comprehensive platform is the go-to resource for both seasoned professionals and passionate DIY enthusiasts. With expert guidance, in-depth reviews, and invaluable tips, Chainsaw Guru helps you make informed choices when selecting the perfect chainsaw. Join their engaging community to connect with like-minded individuals, and explore their handpicked selection of premium products and accessories. Stay updated with the latest industry news and unleash the full potential of your chainsaw endeavors. Check out <a href="https://chainsawguru.com/">Chainsaw Guru Blog</a> and experience the cutting edge of chainsaw expertise. Happy cutting!</p>
+        <div>
+            <a href="https://www.facebook.com/ahsanghaffar23/"><button>Contact</button></a>
+        </div>
+    </div>
+</body>
+
+</html>
